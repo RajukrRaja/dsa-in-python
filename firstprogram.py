870660fc3da8bd0101c02c6d4fb9e8d30bbbdb16
RajukrRaja <rajukumar.191813@gmail.com>
2026-05-30 17:03:12 +0530

    first day

---
+++ firstprogram.py
@@ -0,0 +1,137 @@
+# print("Hello python from this world")
+
+
+#input in python 
+
+# name = input("Enter your name: ")
+# print("Hello" , name )
+
+
+# Addition of two number using user input
+# num = int(input("enter your first number -> "))
+# num1 = int(input("enter your second number -> "))
+
+# sum = num + num1
+# print("your sum => " ,  sum)
+
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+# print("Remainder:", a % b)
+# print("power:", a ** b)
+# print("floor division :", a // b)
+
+
+
+# import math
+
+# radius = float(input("enter a radius"))
+
+# area = math.pi*radius**2
+
+# print("Area of circle is" , area)
+
+
+# for i in range(50):
+#     print(i)
+
+
+
+# num = int(input("Enter a number: "))
+# for i in range(1,125):
+#     print(num , "x", i , "=", num*i)
+
+
+# count = 1
+
+# while count <= 100:
+    
+#     if count % 2 == 0:
+#         print(count)
+
+#     count += 1
+
+
+
+# num = int(input("Enter a Number: "))
+
+# if num > 0:
+#     print("Positive")
+
+# elif num < 0:
+#     print("Negative")
+
+# else:
+#     print("Zero")
+
+
+
+
+# def greet():
+#     print("hello")
+
+
+# greet()
+# greet()
+
+
+# def square(num):
+#     print(num*num)
+
+
+# square(5)
+
+
+
+# def cube(num):
+#     return num*num*num
+# result = cube(3)
+
+# print(result)
+
+
+
+# def largest(a, b):
+
+#     if a > b:
+#         return "a is larger"
+
+#     elif b > a:
+#         return "b is larger"
+
+#     else:
+#         return "both are equal"
+
+
+# result = largest(5, 5)
+
+# print(result)
+
+
+
+
+# list in python
+
+# number = [2, 4, 6, 8]
+
+# for num in number:
+#     print(num)
+
+# print(number[0])
+# print(number[1])
+# print(number)
+
+
+# number = [10, 20 , 30]
+
+# print(max(number))
+# print(sum(number))
+
+
+
